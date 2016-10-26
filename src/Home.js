e1c9bf3b2462b0ae5e38bf0814ec46eeb49aebf3
Alex DeCapri <alexdecapri@gmail.com>
2016-10-26 16:34:26 -0600

    updates to home page

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Segment, Button, Divider } from 'semantic-ui-react'
+import logo from './oso2.svg';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h2>Home</h2>
+
+        <img src={logo} id="oso-logo" alt="logo"/>
+
+        <Segment padded id='login-signup'>
+         <Button primary fluid>Login</Button>
+         <Divider horizontal>Or</Divider>
+         <Button secondary fluid>Sign Up Now</Button>
+        </Segment>
       </div>
     )
   }
