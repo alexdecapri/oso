2494526a2cac61ef5b4b16a722b1665e0a12d41e
Alex DeCapri <alexdecapri@gmail.com>
2016-10-26 16:51:24 -0600

    Mock tests for Tests.js

--- src/Tests.js
+++ src/Tests.js
@@ -1,10 +1,122 @@
 import React, { Component } from 'react';
+import { Image, Item } from 'semantic-ui-react'
+
+let style1 = {
+  marginTop: "20px",
+}
+
+let style2 = {
+  marginLeft: "10px",
+}
 
 class Tests extends Component {
   render() {
     return (
       <div>
-        <h2>Tests</h2>
+      <Item.Group style={style2}>
+        <Item style={style1}>
+          <Item.Image size='tiny' src='http://semantic-ui.com/images/wireframe/image.png' />
+
+          <Item.Content>
+            <Item.Header as='a'>Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
+            </Item.Description>
+            <Item.Extra>Last edit: 16 Days Ago</Item.Extra>
+          </Item.Content>
+        </Item>
+
+        <Item>
+          <Item.Image size='tiny' src='http://semantic-ui.com/images/wireframe/image.png' />
+
+          <Item.Content>
+            <Item.Header as='a'>Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
+            </Item.Description>
+            <Item.Extra>Last Edit: Yesterday</Item.Extra>
+          </Item.Content>
+        </Item>
+
+        <Item>
+          <Item.Image size='tiny' src='http://semantic-ui.com/images/wireframe/image.png' />
+
+          <Item.Content>
+            <Item.Header as='a'>Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
+            </Item.Description>
+            <Item.Extra>Last edit: 16 Days Ago</Item.Extra>
+          </Item.Content>
+        </Item>
+
+        <Item>
+          <Item.Image size='tiny' src='http://semantic-ui.com/images/wireframe/image.png' />
+
+          <Item.Content>
+            <Item.Header as='a'>Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
+            </Item.Description>
+            <Item.Extra>Last Edit: Yesterday</Item.Extra>
+          </Item.Content>
+        </Item>
+        <Item>
+          <Item.Image size='tiny' src='http://semantic-ui.com/images/wireframe/image.png' />
+
+          <Item.Content>
+            <Item.Header as='a'>Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
+            </Item.Description>
+            <Item.Extra>Last edit: 16 Days Ago</Item.Extra>
+          </Item.Content>
+        </Item>
+
+        <Item>
+          <Item.Image size='tiny' src='http://semantic-ui.com/images/wireframe/image.png' />
+
+          <Item.Content>
+            <Item.Header as='a'>Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
+            </Item.Description>
+            <Item.Extra>Last Edit: Yesterday</Item.Extra>
+          </Item.Content>
+        </Item>
+        <Item>
+          <Item.Image size='tiny' src='http://semantic-ui.com/images/wireframe/image.png' />
+
+          <Item.Content>
+            <Item.Header as='a'>Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
+            </Item.Description>
+            <Item.Extra>Last edit: 16 Days Ago</Item.Extra>
+          </Item.Content>
+        </Item>
+
+        <Item>
+          <Item.Image size='tiny' src='http://semantic-ui.com/images/wireframe/image.png' />
+
+          <Item.Content>
+            <Item.Header as='a'>Header</Item.Header>
+            <Item.Meta>Description</Item.Meta>
+            <Item.Description>
+              <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
+            </Item.Description>
+            <Item.Extra>Last Edit: Yesterday</Item.Extra>
+          </Item.Content>
+        </Item>
+      </Item.Group>
+
       </div>
     )
   }
